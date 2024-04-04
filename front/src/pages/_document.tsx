50064690d3a1e75cd82c457afb42e8016c422115
ehgus5825 <ehgus5825@naver.com>
2024-04-04 15:23:31 +0900

    이미지 오류 개선

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta
+					httpEquiv="Content-Security-Policy"
+					content="upgrade-insecure-requests"
+				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<body>
